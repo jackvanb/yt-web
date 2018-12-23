0d7026507b0363c5f81c3683d30a3c533bc5a3eb
Yash Moondhra <ymoondhra@Yashs-MacBook-Pro-3.local>
2018-12-23 16:48:45 -0500

    nav bar for desktop

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,105 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+// import PropTypes from 'prop-types';
+
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+// import Hidden from '@material-ui/core/Hidden';
+
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+//THEME FOR NAV BAR
+const theme = createMuiTheme({
+    palette: {
+        primary: {main: '#ffffff'}//'#fffafa'}
+    },
+    typography: {
+        fontFamily: [
+            'impact', 'sans-serif',
+        ].join(','),
+    }
+});
+
+class Logo extends Component {
+    render() {
+        return(
+            <a className="navDiv1 logo" href="#"> 
+                <IconButton> <div className="logo"></div> </IconButton>
+            </a>
+        );
+    }
+}
+
+class DropDownButtons extends Component {
+    render() {
+        return(
+            <div className="navDiv2 flex">
+                <Button className="navDiv2button" color="primary">Take the Quiz</Button>
+                <Button className="navDiv2button" color="primary">Meet Our Nutritionists</Button>
+                <Button className="navDiv2button" color="primary">Contact Us</Button>
+            </div>
+        );
+    }
+}
+
+class UserAunthenticationButtons extends Component {
+    render() {
+        return(
+            <div className="navDiv3 flex">
+                <Button className="navDiv3button" variant="contained" color="primary">Sign Up</Button>
+                <div className="navDiv3space"> </div>
+                <Button className="navDiv3button" variant="contained" color="primary">Log In</Button>
+            </div>
+        );
+    }
+}
+
+//NAV BAR
+class NavBar extends Component {
+
+    render() {
+        // var first = this.props.width;// > 600 ? <Logo /> : <DropDownButtons />;
+        return(
+            <MuiThemeProvider theme={theme}>
+                <div className="navBackground flex">
+                    <Toolbar className="navBar">
+                        <Logo />
+                        <DropDownButtons />
+                        <UserAunthenticationButtons />
+                    </Toolbar>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
+}
 
 export default class App extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
+        this.state = {width: 0};
+        // this.setSize = this.setSize.bind(this);
     }
+
+    // setSize() {
+    //     this.setState({width: window.innerWidth});
+    // }
+
+    // componentDidMount() {
+    //     this.setSize();
+    //     window.addEventListener('resize', this.setSize);
+    // }
+
+    // componentWillUnmount() {
+    //     window.removeEventListener('resize', this.setSize);
+    // }
 
     render() {
         return (
-            <div className='container'>                
-                <img className='logo' src={logo} />
-                <h1 className='title'>React Starter</h1>
+            <div className='container'>
+                <NavBar width={this.state.width}></NavBar>
             </div>
         );
     }
