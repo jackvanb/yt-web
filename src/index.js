64ccd680414231c79b73293fb71ca0d69ff0c2ba
Jack Van Boening <jackvanboening@gmail.com>
2019-02-01 10:42:43 -0800

    Add quiz page (#3)

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
 // PACKAGE DEPENDENCIES
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 // STYLES
 
-import 'normalize.css';
-import './styles/app.scss';
+import "normalize.css";
+import "./styles/app.scss";
 
-import App from './components/App.js';
+import App from "./components/App.js";
 
-import $ from 'jquery';
+import $ from "jquery";
 
 // You can choose your kind of history here (e.g. browserHistory)
 // Your routes.js file
 // import routes from './routes';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('app')
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("app")
 );
 
-
-
-$(window).scroll(function()
-{
-    var scrolled = $(window).scrollTop();
-    $('#background').css('top',(0-(scrolled*0.25))+'px');
+$(window).scroll(function() {
+  var scrolled = $(window).scrollTop();
+  $("#background").css("top", 0 - scrolled * 0.25 + "px");
 });
